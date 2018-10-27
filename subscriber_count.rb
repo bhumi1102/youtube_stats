675a39a4a907febbfa7a83f4a35e4cd0116240c5
bhumi1102 <bshah@castlighthealth.com>
2018-10-26 23:07:37 -0700

    add google youtube api sample call

--- subscriber_count.rb
+++ subscriber_count.rb
@@ -1,1 +1,5 @@
 require 'httparty'
+
+response = HTTParty.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCdx5Dk3EWTe2i8YDA7bfl6g&key=AIzaSyBy0m59xgs1E6HaxbiQgJNYgv0hV0NwKMo')
+
+puts response.body, response.code, response.message, response.headers.inspect
